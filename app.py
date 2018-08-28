d70f4b2e96c321d8508eb9d9a5d8a9b8956332dd
Yu-Cheng Liang <a135246a135246@gmail.com>
2018-08-28 15:41:18 +0800

    Add user register

--- app.py
+++ app.py
@@ -11,14 +11,21 @@ def index():
     return render_template('index.html')
 
 @app.route("/login", methods=['POST'])
+def get_record():
+    req_data = request.form
+    username = req_data['username']
+    password = req_data['password']
+    # compare with db
+    return 'User ' + username + ' login with password ' + password, 200
+
+@app.route("/register", methods=['POST'])
 def add_record():
     req_data = request.form
     username = req_data['username']
     password = req_data['password']
-    # record = Record(name = name, cost = cost)
-    # db.session.add(record)
-    # db.session.commit()
-    return 'User ' + username + ' login with password ' + password, 200
+    # put data into db
+    return 'User ' + username + ' register with password ' + password, 200
+
 
 if __name__ == '__main__' :
     app.run(host = 'localhost', port = '5000', debug = False)
