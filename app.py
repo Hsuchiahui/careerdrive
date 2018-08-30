8086287c359f22a36a90bff7ad4a9eb54b308a26
Hsuchiahui <a22923509@gmail.com>
2018-08-30 16:31:51 +0800

    add oauth function

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
-from flask import Flask, request, jsonify, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from datetime import datetime
+#!/usr/bin/python3
+
+from flask import Flask
+from flask import request
+from flask import redirect
+from flask import Response
+
+from oauth2client.client import OAuth2WebServerFlow
+from googleapiclient.discovery import build
 
 app = Flask(__name__)
 
-# views
-@app.route("/")
-def index():
-    return render_template('index.html')
+flow = OAuth2WebServerFlow(client_id='',
+        client_secret='',
+        scope='https://www.googleapis.com/auth/userinfo.email',
+        redirect_uri='')
 
-@app.route("/login", methods=['POST'])
-def get_record():
-    req_data = request.form
-    username = req_data['username']
-    password = req_data['password']
-    # compare with db
-    return 'User ' + username + ' login with password ' + password, 200
 
-@app.route("/register", methods=['POST'])
-def add_record():
-    req_data = request.form
-    username = req_data['username']
-    password = req_data['password']
-    # put data into db
-    return 'User ' + username + ' register with password ' + password, 200
+@app.route('/', methods=['GET'])
+def readfile_handler():
+    auth_uri = flow.step1_get_authorize_url()
+    return redirect(auth_uri)
 
+@app.route('/oauth2callback')
+def oauth_callback():
+    auth_code = request.args.get('code')
+    credentials = flow.step2_exchange(auth_code)
+    service = build('oauth2', 'v2', credentials=credentials)
+    return Response(service.userinfo().get().execute())
 
 if __name__ == '__main__' :
-    app.run(host = 'localhost', port = '5000', debug = False)
+    app.run(host = 'hmkrl.com', port = '8000', debug = False)
